06562e5a8edbddb30bc3f7f49a39176d6d46ba5f
Claire Miller <clairemiller@Claires-MacBook-Air.local>
2017-07-10 18:53:44 -0500

    fix app

--- app.py
+++ app.py
@@ -86,14 +86,14 @@ def input():
         print request.form
         if 'lines1' in request.form:
             app.vars['line1'] = request.form['lines1']
-            print 'vars', app.vars
+            print 'vars1', app.vars
             return render_template('input2.html',form=form,line1=app.vars['line1'])
         elif 'lines2' in request.form:
-            print 'vars', app.vars
             app.vars['line2'] = request.form['lines2']
             app.stops1 = pd.DataFrame(app.stops[app.stops.lines.map(lambda x: app.vars['line1'] in x)])
             app.stops2 = pd.DataFrame(app.stops[app.stops.lines.map(lambda x: app.vars['line2'] in x)])
-            print 'stops1', app.stops1
+            #print 'stops1', app.stops1
+            print 'vars2', app.vars
             return render_template('input3.html',line1=app.vars['line1'],line2=app.vars['line2'],data1=app.stops1['stop_name'],
                                    data2=app.stops2['stop_name'],form=form,hours=hours,minutes=minutes,am_pm=am_pm)
         elif 'start_station' in request.form:
@@ -111,7 +111,10 @@ def input():
             app.vars['end_id'] = app.stops2[app.stops2['stop_name'] == request.form['end_station']]['stop_id'].values[0]
             app.vars['titles'] = ['Depart from %s'%app.vars['start_name'],'Arrive at Transfer','Transfer From','Transfer Wait Time (mintues)',
                                   'Transfer Crowdedness','Transfer To', 'Depart from Transfer', 'Arrive at %s'%app.vars['end_name'], 'Total Time (minutes)']
+            print 'vars3', app.vars
             return redirect('/direct')
+        else:
+            print 'none'
 
 @app.route('/direct', methods=['GET','POST'])
 def direct():
